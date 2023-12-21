9d46a9ba9621e16e02eeede34cffa134f4c81523
onfranciis <onukwuf@gmail.com>
2023-12-21 13:20:15 +0100

    feat: add ThirdPart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "@/styles/page.module.scss";
 import NavBar from "@/components/navbar/NavBar";
 import FirstPart from "@/components/firstpart/FirstPart";
 import SecondPart from "@/components/secondpart/SecondPart";
+import ThirdPart from "@/components/thirdpart/ThirdPart";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <div className={styles.ExcludeNav}>
         <FirstPart />
         <SecondPart />
+        <ThirdPart />
       </div>
     </main>
   );
