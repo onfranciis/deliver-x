f442821a8e2b09912f3b1f2a4ec39e7aa8ef7fd1
onfranciis <onukwuf@gmail.com>
2023-12-21 11:42:00 +0100

    feat: create NavBar

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/NavBar.module.scss";
+
+const NavBar = () => {
+  return (
+    <nav className={styles.NavBar}>
+      <Image
+        src="/logo.svg"
+        alt=""
+        height={100}
+        width={100}
+        className={styles.Logo}
+      />
+
+      <div className={styles.Links}>
+        <Link href="">
+          <p>Home</p>
+        </Link>
+        <Link href="">
+          <p>About</p>
+        </Link>
+        <Link href="">
+          <p>Pricing</p>
+        </Link>
+        <Link href="">
+          <p>Pages</p>
+        </Link>
+
+        <div className={styles.RightActions}>
+          <Link href="">
+            <p>Cart(0)</p>
+          </Link>
+
+          <Link href="">
+            <button className={styles.Download}>Download app</button>
+          </Link>
+
+          <Link href="">
+            <button className={styles.Register}>Register</button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
