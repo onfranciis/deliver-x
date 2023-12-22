cde27ace1d1d6a4f5751b3c71cae96a7926db91c
onfranciis <onukwuf@gmail.com>
2023-12-22 17:31:21 +0100

    fix: update NavBarRWD

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -11,60 +11,63 @@ const NavBar = () => {
   const [burgerClicked, setBurgerClicked] = useState(false);
 
   return (
-    <div className={styles.Parent}>
-      <nav className={styles.NavBar}>
-        <Image
-          src="/logo.svg"
-          alt=""
-          height={100}
-          width={100}
-          className={styles.Logo}
-        />
+    <>
+      <NavBarRWD burgerClicked={burgerClicked} />
+      <div className={styles.Parent}>
+        <nav className={styles.NavBar}>
+          <Image
+            src="/logo.svg"
+            alt=""
+            height={100}
+            width={100}
+            className={styles.Logo}
+          />
 
-        <div className={styles.Links}>
-          <Link href="">
-            <p>Home</p>
-          </Link>
-          <Link href="">
-            <p>About</p>
-          </Link>
-          <Link href="">
-            <p>Pricing</p>
-          </Link>
-          <div className={styles.Pages}>
+          <div className={styles.Links}>
             <Link href="">
-              <p>Pages</p>
-              <span>{">"}</span>
+              <p>Home</p>
             </Link>
-            <PopUp />
-          </div>
-
-          <div className={styles.RightActions}>
             <Link href="">
-              <p>Cart(0)</p>
+              <p>About</p>
             </Link>
-
             <Link href="">
-              <button className={styles.Download}>Download app</button>
+              <p>Pricing</p>
             </Link>
+            <div className={styles.Pages}>
+              <Link href="">
+                <p>Pages</p>
+                <span>{">"}</span>
+              </Link>
+              <PopUp />
+            </div>
 
-            <Link href="">
-              <button className={styles.Register}>Register</button>
-            </Link>
+            <div className={styles.RightActions}>
+              <Link href="">
+                <p>Cart(0)</p>
+              </Link>
 
-            <div
-              className={`${styles.Burger} ${burgerClicked && styles.Selected}`}
-              onClick={() => setBurgerClicked((data) => !data)}
-            >
-              <span className={styles.One}></span>
-              <span className={styles.Two}></span>
+              <Link href="">
+                <button className={styles.Download}>Download app</button>
+              </Link>
+
+              <Link href="">
+                <button className={styles.Register}>Register</button>
+              </Link>
+
+              <div
+                className={`${styles.Burger} ${
+                  burgerClicked && styles.Selected
+                }`}
+                onClick={() => setBurgerClicked((data) => !data)}
+              >
+                <span className={styles.One}></span>
+                <span className={styles.Two}></span>
+              </div>
             </div>
           </div>
-        </div>
-      </nav>
-
-      <NavBarRWD burgerClicked={burgerClicked} />
-    </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
